fe6a280aac2b389d9149c03283b3a65a461f8d1e
EC2 Default User <ec2-user@ip-10-112-38-121.ec2.internal>
2013-08-18 05:18:45 +0000

    its working

--- server.js
+++ server.js
@@ -1,22 +1,40 @@
-var simplesmtp = require("simplesmtp"),
-    fs = require("fs");
+//console.log(process.stdout.writable);
+var simplesmtp = require("simplesmtp");
+var MailParser = require("mailparser").MailParser;
+var request = require('request');
+var cheerio = require('cheerio');
 
-var smtp = simplesmtp.createServer();
-smtp.listen(3000);
-
-smtp.on("startData", function(connection){
-    console.log("Message from:", connection.from);
-    console.log("Message to:", connection.to);
-    connection.saveStream = fs.createWriteStream("/tmp/message.txt");
+simplesmtp.createSimpleServer({SMTPBanner:"Sendmail, sure."}, function(req){
+    process.stdout.write("\r\nNew Mail:\r\n");
+mailparser = new MailParser();
+mailparser.on("end", function(mail_object){
+console.log("### received email "+req.from+" > "+req.to+":\n -"+mail_object.subject);
+if (mail_object.html) {
+  $ = cheerio.load(mail_object.html);
+  links = $('a'); 
+  $(links).each(function(i, link){
+    var url = $(link).attr('href');
+    console.log("# Found link: "+ $(link).text() + ':  ' + url);
+request(url, function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    console.log("# Visited: "+url);
+  } else {
+    console.error("# Error on "+url+" :"+ error);
+  }
+})
+  
 });
 
-smtp.on("data", function(connection, chunk){
-    connection.saveStream.write(chunk);
-});
 
-smtp.on("dataReady", function(connection, callback){
-    connection.saveStream.end();
-    console.log("Incoming message saved to /tmp/message.txt");
-    callback(null, "ABC1"); // ABC1 is the queue id to be advertised to the client
-    // callback(new Error("Rejected as spam!")); // reported back to the client
+};
+});
+req.pipe(mailparser);
+    req.accept();
+}).listen(25, function(err){
+    if(!err){
+        console.log("SMTP server listening on port 25");
+    }else{
+        console.log("Could not start server on port 25. Ports under 1000 require root privileges.");
+        console.log(err.message);
+    }
 });

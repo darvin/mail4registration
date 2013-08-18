40659b752a94451cce259b0cc1c425f353e871b6
Sergey Klimov <sergey.v.klimov@gmail.com>
2013-08-17 23:40:12 -0700

    beautified

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 mailserver-auto-verify
 ======================
+Mail server that accepts any inbound mail and clicks on any links in body of mail
+
+Just run:
+
+    npm install
+    node server.js
+
+And enjoy autoverifying of any incoming mail with registrations on spam services! It's like 10 minutes mail but much better.
--- server.js
+++ server.js
@@ -4,36 +4,38 @@ var MailParser = require("mailparser").MailParser;
 var request = require('request');
 var cheerio = require('cheerio');
 
-simplesmtp.createSimpleServer({SMTPBanner:"Sendmail, sure."}, function(req){
+simplesmtp.createSimpleServer({
+    SMTPBanner: "Sendmail, sure."
+}, function (req) {
     process.stdout.write("\r\nNew Mail:\r\n");
-mailparser = new MailParser();
-mailparser.on("end", function(mail_object){
-console.log("### received email "+req.from+" > "+req.to+":\n -"+mail_object.subject);
-if (mail_object.html) {
-  $ = cheerio.load(mail_object.html);
-  links = $('a'); 
-  $(links).each(function(i, link){
-    var url = $(link).attr('href');
-    console.log("# Found link: "+ $(link).text() + ':  ' + url);
-request(url, function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-    console.log("# Visited: "+url);
-  } else {
-    console.error("# Error on "+url+" :"+ error);
-  }
-})
-  
-});
+    mailparser = new MailParser();
+    mailparser.on("end", function (mail_object) {
+        console.log("### received email " + req.from + " > " + req.to + ":\n -" + mail_object.subject);
+        if (mail_object.html) {
+            $ = cheerio.load(mail_object.html);
+            links = $('a');
+            $(links).each(function (i, link) {
+                var url = $(link).attr('href');
+                console.log("# Found link: " + $(link).text() + ':  ' + url);
+                request(url, function (error, response, body) {
+                    if (!error && response.statusCode == 200) {
+                        console.log("# Visited: " + url);
+                    } else {
+                        console.error("# Error on " + url + " :" + error);
+                    }
+                })
+
+            });
 
 
-};
-});
-req.pipe(mailparser);
+        };
+    });
+    req.pipe(mailparser);
     req.accept();
-}).listen(25, function(err){
-    if(!err){
+}).listen(25, function (err) {
+    if (!err) {
         console.log("SMTP server listening on port 25");
-    }else{
+    } else {
         console.log("Could not start server on port 25. Ports under 1000 require root privileges.");
         console.log(err.message);
     }

6978faa333f004ccae802ecef3c412b35261c597
Sergey Klimov <sergey.v.klimov@gmail.com>
2013-08-19 23:20:20 -0700

    persistance of domains

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ var simplesmtp = require("simplesmtp");
 var MailParser = require("mailparser").MailParser;
 var request = require('request');
 var cheerio = require('cheerio');
-
+var express = require('express');
+var dirty = require('dirty');
 
 var linkInHtml = function(html, callback) {
     $ = cheerio.load(html);
@@ -17,7 +18,9 @@ var linkInHtml = function(html, callback) {
 var linkRegex =/(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
 
 var linkInText = function(text, callback) {
-    var result = text.match(linkRegex)[0];
+    var match = text.match(linkRegex);
+
+    var result = match? match[0] : null;
     callback(null, result);
 };
 
@@ -34,32 +37,76 @@ var followUrl = function(url, callback) {
             callback(error);
     })
 }
+
+
+
+
 module.exports = function(opts) {
-   simplesmtp.createSimpleServer({
-        SMTPBanner: "Sendmail, sure."
-    }, function (req) {
-        process.stdout.write("\r\nNew Mail:\r\n");
-        mailparser = new MailParser();
-        mailparser.on("end", function (mail_object) {
-            console.log("### received email " + req.from + " > " + req.to + ":\n -" + mail_object.subject);
-            if (mail_object.html) {
-                linkInHtml(mail_object.html, function(err, url){
-                    followUrl(url);
-                });
-            } else if (mail_object.text) {
-                linkInText(mail_object.text, function(err, url){
-                    followUrl(url);
-                });
-            };
+    var db = null;
+    var domains = {};
+    function initDB(callback) {
+        db = dirty(opts.db);
+        db.on('load', function() {
+            db.forEach(function(domainName,domainData){
+                domains[domainName]=domainData;
+            });
+            db.on('drain', function() {
+                console.log('All records are saved on disk now.');
+            });
+
+            callback();
         });
-        req.pipe(mailparser);
-        req.accept();
-    }).listen(opts.smtpPort, function (err) {
-        if (!err) {
-            console.log("SMTP server listening on port "+opts.smtpPort);
-        } else {
-            console.log("Could not start server on port "+opts.smtpPort+". Ports under 1000 require root privileges.");
-            console.log(err.message);
+    }
+    function addDomainFromIncomingEmail(email, callback){
+        var domainName = email.replace(/.*@/, "");
+        if (domainName) {
+            domains[domainName] = {updated: new Date()};
+            db.set(domainName, domains[domainName], function(){
+                if (callback)
+                    callback();
+            });
         }
-    }); 
+    }
+
+
+    function createApiServer(callback){
+        var app = express();
+        var server = null;
+        app.get("/domains",function(req, res){
+          res.json({domains:Object.keys(domains)});
+        });
+        server = app.listen(opts.apiPort, callback);
+
+    }
+    function createMailServer(callback){
+        simplesmtp.createSimpleServer({
+            SMTPBanner: "Sendmail, sure."
+        }, function (req) {
+            process.stdout.write("\r\nNew Mail:\r\n");
+            mailparser = new MailParser();
+            mailparser.on("end", function (mail_object) {
+                console.log("### received email " + req.from + " > " + req.to + ":\n -" + mail_object.subject);
+                addDomainFromIncomingEmail(req.to[0]);
+                if (mail_object.html) {
+                    linkInHtml(mail_object.html, function(err, url){
+                        if (url)
+                            followUrl(url);
+                    });
+                } else if (mail_object.text) {
+                    linkInText(mail_object.text, function(err, url){
+                        if (url)
+                            followUrl(url);
+                    });
+                };
+            });
+            req.pipe(mailparser);
+            req.accept();
+        }).listen(opts.smtpPort, callback); 
+    }
+    initDB(function(){
+        createMailServer(function(){
+            createApiServer()
+        })
+    });
+
 }
--- test/server_test.js
+++ test/server_test.js
@@ -1,17 +1,23 @@
 var chai = require("chai");
 var expect = chai.expect;
 var should = chai.should();
+var fs = require('fs');
+var request = require('request');
 var mailserver = require("../");
 var MAIL_SERVER_SMTP_PORT=2500;
 var MAIL_SERVER_API_PORT=8000;
 var MAIL_SERVER_HOST="localhost";
+var MAIL_SERVER_DB = "/tmp/test_domains.db"
 var MAIL_SERVER_FIRST_DOMAIN="mail4registration.com"
 var FAKE_SERVER_URL="http://localhost:8080/confirm_registration?email=";
+var SERVER_DOMAIN_LIST_URL="http://"+MAIL_SERVER_HOST+":"+MAIL_SERVER_API_PORT+"/domains";
 var email   = require("emailjs");
 
 
 function cleanDB(done){
-	done();
+	fs.unlink(MAIL_SERVER_DB, function(err) {
+		done();
+	});
 }
 
 var fakeServer = function(callback, receivedCallback) {
@@ -35,9 +41,12 @@ describe('Mail4Registration server', function(){
 	var m = null;
 	var client = null;
 	before(function(done) {
-		//run fake server
 		//run email server
-		m = mailserver({smtpPort:MAIL_SERVER_SMTP_PORT, apiPort:MAIL_SERVER_API_PORT});
+		m = mailserver({
+			smtpPort:MAIL_SERVER_SMTP_PORT, 
+			apiPort:MAIL_SERVER_API_PORT,
+			db:MAIL_SERVER_DB
+		});
 		client  = email.server.connect({
 		   host:    MAIL_SERVER_HOST, 
 		   port:    MAIL_SERVER_SMTP_PORT
@@ -45,7 +54,16 @@ describe('Mail4Registration server', function(){
 
 		cleanDB(done);
 	});
-	it('should have one server in registry initially');
+	it('should have no one server in registry initially', function(done){
+		request({url:SERVER_DOMAIN_LIST_URL, json:true}, function (error, response, body) {
+			expect(error).to.be.null;
+			response.statusCode.should.equal(200);
+			console.error(body);
+			expect(body.domains).to.be.eql([]);
+			done();
+		});
+
+	});
 	it('should follow the links in html emails', function(done){
 		var email = "someone@"+MAIL_SERVER_FIRST_DOMAIN;
 
@@ -84,7 +102,25 @@ describe('Mail4Registration server', function(){
 			done(err);
 		});
 	})
-	it('should have add servers from incoming emails to registry');
-	it('should preserve server registry after restart');
+	it('should add servers from incoming emails to registry', function(done) {
+		var NEW_DOMAIN = "newdomain.org";
+		var DOMAINS_WITH_NEW_DOMAIN = [MAIL_SERVER_FIRST_DOMAIN];
+		DOMAINS_WITH_NEW_DOMAIN.push(NEW_DOMAIN);
+		client.send({ subject: "1", from: "you <username@gmail.com>", 
+			to: "someperson@"+NEW_DOMAIN, text: "2" }, 
+			function(err, message) {
+				request({url:SERVER_DOMAIN_LIST_URL, json:true}, function (error, response, body) {
+					response.statusCode.should.equal(200);
+					expect(body.domains).to.be.eql(DOMAINS_WITH_NEW_DOMAIN);
+					done();
+				});
+
+			});
+
+
+	});
+	it('should preserve server registry after restart', function(done){
+		setTimeout(done, 3000);
+	});
 	after(cleanDB);
 })
